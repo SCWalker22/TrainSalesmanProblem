840015e129b8e3d136006422fdb126b47b73c170
SCWalker22 <stanthewalker1@gmail.com>
2025-10-15 18:23:18 +0100

    Added more code for branch based solving, need to set up proper data structures and looping to explore all branches, and preserve all information. Should make this code as adaptable as possible, so we can adpat it for a* based algorithm

--- TSPSolver.py
+++ TSPSolver.py
@@ -55,15 +55,15 @@ def fill_first_arrival_table(
                     pl.when(
                         (pl.col("arrival").is_null()) | (pl.col("arrival") > pl.col("firstArrival"))
                 )
-                .then(pl.col("firstArrival"))
-                .otherwise(pl.col("arrival"))
-                .alias("arrival"),
+                        .then(pl.col("firstArrival"))
+                        .otherwise(pl.col("arrival"))
+                        .alias("arrival"),
                     pl.when(
                         (pl.col("arrival").is_null()) | (pl.col("arrival") > pl.col("firstArrival"))
                     )
-                .then(pl.lit(station))
-                .otherwise(pl.col("serviceFrom"))
-                .alias("serviceFrom")
+                        .then(pl.lit(station))
+                        .otherwise(pl.col("serviceFrom"))
+                        .alias("serviceFrom")
             ]
                 ).select(["crs", "arrival", "serviceFrom"])
     )
@@ -84,7 +84,9 @@ def find_timed_stations(
     services_via_start = (services.join(valid_services_out, on="serviceUid")
                         .filter((pl.col("crs") != station_start) & (pl.col("time") < pl.col("arrival")))
                         )
-    services_via_start = services_via_start.with_columns(pl.when(pl.col("nextDay") == "true").then(pl.col("arrival").cast(pl.Int64) + 2400).otherwise(pl.col("arrival")).alias("arrival")).sort(by="arrival")
+    services_via_start = services_via_start.with_columns(pl.when(pl.col("nextDay") == "true")
+                                                        .then(pl.col("arrival").cast(pl.Int64) + 2400)
+                                                        .otherwise(pl.col("arrival")).alias("arrival")).sort(by="arrival")
     arrivals = fill_first_arrival_table(arrivals, services_via_start, station_start)
     return arrivals
 
@@ -121,7 +123,7 @@ def find_first_arrival(
                 # services_from_station = services.filter(pl.col("crs") == station & pl.col("departure") > departure_time)
                 new_start_time = datetime(2025, 9, 1, departure_time//100, departure_time%100, 0)
                 arrivals = find_timed_stations(services, station, arrivals, new_start_time)
-            arrival_time: int | None = arrivals.filter(pl.col("crs") == station_end)["arrival"][0]
+            arrival_time = arrivals.filter(pl.col("crs") == station_end)["arrival"][0]
 
             if arrival_time is not None:
                 extra_checks += 1
@@ -176,7 +178,7 @@ def find_minimum_spanning_tree(
     min_spanning_tree_sparse = minimum_spanning_tree(adjacency_matrix)
     min_spanning_tree_dense = min_spanning_tree_sparse.toarray()
 
-    rows: dict[str, str | int] = {"stationStart": stations}
+    rows: dict[str, list[str] | int] = {"stationStart": stations}
     for i, col in enumerate(stations):
         rows[col] = min_spanning_tree_dense[:, i].tolist()
     # rows = [[dm["stationStart"][i]] + list(min_spanning_tree_dense[i]) for i in range(len(stations))]
@@ -185,6 +187,30 @@ def find_minimum_spanning_tree(
 
     min_span_tree = min_span_tree.with_columns([pl.col(col).cast(pl.Int64) for col in stations])
     return min_span_tree, stations
+
+def organise_arrivals_df(
+    arrivals: pl.DataFrame,
+    arrivals_new: list[pl.DataFrame]
+
+    ) -> pl.DataFrame:
+    return arrivals
+
+def find_possible_next_stations(
+    services: pl.DataFrame,
+    arrivals: dict[str, dict[list[str, datetime]]],
+    # start_time: datetime,
+    # change_time: int = 5
+
+    ) -> pl.DataFrame:
+    """
+
+    """
+    arrivals_new: list[pl.DataFrame] = []
+    for start_station, details in arrivals.items():
+        arrivals_temp = find_timed_stations(services, start_station, pl.DataFrame({"crs": all_stations, "arrival": None, "serviceFrom": None}), details["arrival"])
+        arrivals_new.append(arrivals_temp)
+    arrivals_sorted = organise_arrivals_df(arrivals, arrivals_new)
+    return arrivals_sorted
 
 def christofides(
     services: pl.DataFrame,
@@ -236,9 +262,89 @@ def a_star_solver(
     """
     
     """
+    
+    return start_time, []
+    
+def find_next_branch(
+    services: pl.DataFrame,
+    stations_remaining: list[str],
+    stations_reached: dict[str, dict[str, list[str] | datetime]],
+    start_station: str,
+    change_time: int = 5
+
+    ) -> tuple[list[str], dict[str, dict[str, list[str] | datetime]]]:
+    """
+    Return a list pf remaining station, dict of how to get to reached stations - Should also contain incorrect ones
+    So we can search after this
+    Need to properly implement branching
+    """
+    routes: list[tuple[list[str], datetime]] = []
+    none_reachable = True
+    for station in stations_remaining:
+        print(stations_reached)
+        if station in stations_reached.keys():
+            # We have found a station, we would like to go here - also need to handle branching if we can go to a station
+            time_reached = stations_reached[station]["arrival"]
+            hours = time_reached//100
+            mins = (time_reached%60) + change_time
+            print(f"{station} here")
+            routes.append(([start_station, station], datetime(2025, 9, 1, hours, mins, 0)))
+            none_reachable = False
+    for station in routes:
+        stations_remaining.remove(station[0][1])
+    if none_reachable:
+        # find new route
+        arrivals_df = find_possible_next_stations(services, arrivals)
+        print("here")
+    print(routes)
+    print(stations_remaining)
+    return stations_remaining, routes
+
+def branching_graph(
+        services: pl.DataFrame,
+        stations: list[str],
+        start_time: datetime,
+        start_station: str = "",
+        change_time: int = 5
+
+    ) -> list[tuple[datetime, list[str]]]:
+    """
+    
+    """
     if start_station == "":
         start_station = stations[0]
-    pass
+    all_stations = services["crs"].unique().to_list()
+    # stations_reachable: dict[str, int] = {}
+    # stations_reached: dict[str, int] = {}
+    stations_reachable: pl.DataFrame = pl.DataFrame({"crs": all_stations, "arrival": None, "serviceFrom": None})
+    stations_reached: dict[str, dict[str, list[str] | int]] = {}
+    stations_reachable_new = find_timed_stations(services, start_station, stations_reachable, start_time)
+    stations_reached_df = stations_reachable_new.filter(pl.col("arrival").is_not_null())
+    stations_reached = {row["crs"]: {"arrival": row["arrival"], "serviceFrom": [row["serviceFrom"]]} for row in stations_reached_df.iter_rows(named=True)}
+    # 2 Cases now, we have a possible station in our list, or we don't, and need to search for another
+    stations_remaining = stations.copy()
+    arrivals = stations_reached
+    while len(stations_remaining) != 0:
+        stations_remaining, new_routes = find_next_branch(services, stations_remaining, arrivals, start_station, change_time)
+        print(arrivals)
+        print(stations_remaining)
+    # none_reachable = True
+    # for station in stations_remaining:
+    #     print(station)
+    #     if station in stations_reached.keys():
+    #         # We have found a station, we would like to go here - also need to handle branching if we can go to a station
+    #         time_reached = stations_reached[station]["arrival"]
+    #         hours = time_reached//100
+    #         mins = (time_reached%60) + change_time
+    #         print(f"{station} here")
+    #         routes.append(([start_station, station], datetime(2025, 9, 1, hours, mins, 0)))
+    #         none_reachable = False
+    # if none_reachable:
+    #     # find new route
+    #     arrivals_df = find_possible_next_stations(services, arrivals)
+    #     print("here")
+    print(routes)
+        
 
 def simple_route_finder(
         services: pl.DataFrame,
@@ -247,7 +353,7 @@ def simple_route_finder(
         dm: pl.DataFrame,
         change_time: int = 5
 
-    ) -> tuple[list[str], int]:
+    ) -> tuple[list[str], float]:
     """
     
     """
@@ -261,11 +367,11 @@ def a_star(
         start_time: datetime,
         change_time: int = 5
 
-    ) -> tuple[list[str], int]:
+    ) -> tuple[list[str], float]:
     """
     
     """
-    return [], 0
+    return [], 0.0
 
 def simple_improved(
         services: pl.DataFrame,
@@ -291,24 +397,19 @@ def full_graph_traversal(
     Find some possible routes, and return - these may not be the quickest, but should contain the quickest
     """
     for start_station in stations:
-        valid_services = services.filter((pl.col("serviceUid").is_in(services.filter((pl.col("crs") == start_station) &
-                                                                    (pl.col("departure") > int(start_time.strftime("%H%M"))))["serviceUid"].to_list()))
-                                        &
-                                        (pl.col("departure") > int(start_time.strftime("%H%M")))) # Check they are correct direction
-        # valid_services_out = valid_services_out.with_columns(pl.col("departure").alias("time")).select(["serviceUid", "time"])
-        # services_via_start = (services.join(valid_services_out, on="serviceUid")
-        #                     .filter((pl.col("crs") != station_start) & (pl.col("time") < pl.col("arrival")))
-        #                     )
+        route_end_time, route = branching_graph(services, stations, start_time, start_station)
     return [[]], [0]
 
 if __name__ == "__main__":
     start_time = time.time()
     services = convert_cols_to_numeric(rename_cols(pl.read_csv("Services.csv", infer_schema=None)))
-    stations = ['MYB', 'BDS', 'CST', 'CHX', 'CTK', 'EPH', 'EUS', 'ZFD', 'FST', 'HOX', 'KGX', 'LST', 'LBG', 'MOG', 'OLD', 'PAD', 'SDC', 'STP', 'TCR', 'VXH', 'VIC', 'BFR', 'WAT', 'WAE']
-    stations_to_travel = christofides(services, stations, dm=pl.read_csv("ConnectionTimes.csv", infer_schema=None))
-    journey_time, route = get_full_route_timed(services, stations_to_travel)
+    # stations = ['MYB', 'BDS', 'CST', 'CHX', 'CTK', 'EPH', 'EUS', 'ZFD', 'FST', 'HOX', 'KGX', 'LST', 'LBG', 'MOG', 'OLD', 'PAD', 'SDC', 'STP', 'TCR', 'VXH', 'VIC', 'BFR', 'WAT', 'WAE']
+    # stations_to_travel = christofides(services, stations, dm=pl.read_csv("ConnectionTimes.csv", infer_schema=None))
+    # journey_time, route = get_full_route_timed(services, stations_to_travel)
+    stations = ["EXD", "IPS", "EXC", "PLY"]
+    journey_time, route = full_graph_traversal(services, stations, datetime(2025, 9, 1, 0, 0, 0))
     end_time = time.time()
     time_taken = end_time - start_time
-    print(f"initial route {stations_to_travel}")
+    # print(f"initial route {stations_to_travel}")
     print(journey_time, route)
     print(f"Took {time_taken} seconds")
